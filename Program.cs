c6252917ab8863773fa035c09618a0cd9888be48
Stanislav Kuznetsov <stanislav3012@list.ru>
2022-09-04 12:59:46 +0300

    homework 7

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble();
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++ )
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]:N3} "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+int[,] intoArray = new int[3, 4]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+Console.Write("Введите индекс строки искомого элемента: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите индекс столбца: ");
+int columns = int.Parse(Console.ReadLine());
+
+
+if (intoArray.GetLength(0)>=rows && intoArray.GetLength(1)>=columns)
+{
+    Console.WriteLine($"по позиции массива [{rows}:{columns}] число {intoArray[rows, columns]}");
+}
+else
+{
+    Console.WriteLine($"[{rows}:{columns}] -> такого числа в массиве нет");
+}
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = new int[3, 4]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+Console.Write("Среднее арифметическое каждого столбца: ");
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j,i];
+        }
+
+    Console.Write ($"{Math.Round(sum/array.GetLength(0),2)} ");
+}
